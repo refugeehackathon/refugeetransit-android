b46daf382fc7245f3ab0727cf5e81e4a9877f097
Tobias Preuss <tobias.preuss@googlemail.com>
2015-10-24 16:43:39 +0200

    Apply formatting from Android Studio.

--- app/src/main/java/de/refugeehackathon/transit/MainActivity.java
+++ app/src/main/java/de/refugeehackathon/transit/MainActivity.java
@@ -1,16 +1,12 @@
 package de.refugeehackathon.transit;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
-import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
 
@@ -24,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        
+
         mMapView = (MapView) findViewById(R.id.mapview);
         mMapView.setTileSource(TileSourceFactory.MAPNIK);
 
